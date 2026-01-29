7041402be8dec90faa571a4ed12b76f272d71ee9
vinicius <vmm_7@hotmail.com>
2026-01-29 12:01:27 -0300

    feat: cria camada de serviço que retorna um user para o controller

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,7 +1,15 @@
+/*
+  A camada de serviço (service layer) é responsável por conter a lógica de negócio da aplicação.
+  Ela atua como um intermediário entre a camada de controle (controllers) e a camada de acesso a dados (repositories).
+  Essa separação de responsabilidades promove um código mais organizado, testável e de fácil manutenção.
+*/
 import userRepositories from "../repositories/user.repositories.js";
 
-function createUserService(newUser) {
-  return userRepositories.createUserRepository(newUser);
+// Regras de negócio para criação de usuário
+// A função createUserRepository gera, retorna uma Promise ("resolve" ou "reject" da promessa)
+async function createUserService(newUser) {
+  const user = await userRepositories.createUserRepository(newUser);
+  return user;
 }
 
 export default { createUserService };
