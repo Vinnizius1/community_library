e1e65d892ebcebfd77bcbce9a44dff9d4ee3ea8b
vinicius <vmm_7@hotmail.com>
2026-02-05 11:37:30 -0300

    refactor: migrate database from sqlite3 to postgresql

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,5 @@
+/* CÓDIGO ANTIGO:
+
 import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database("library_db.sqlite", (err) => {
@@ -7,5 +9,32 @@ const db = new sqlite3.Database("library_db.sqlite", (err) => {
     console.log("Connected to the SQLite database.");
   }
 });
+
+export default db; */
+
+// CÓDIGO NOVO:
+
+import pg from "pg";
+import "dotenv/config";
+
+const { Pool } = pg;
+
+// O Pool gerencia várias conexões ao banco de dados automaticamente
+const db = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+// Teste de conexão inicial
+db.connect()
+  .then(() => {
+    console.log("✅ Conectado ao PostgreSQL com sucesso!");
+  })
+  .catch((err) => {
+    console.error("❌ Erro ao conectar no Postgres:", err.stack);
+  });
 
 export default db;
