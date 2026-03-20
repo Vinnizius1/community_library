43c6a878b24c319171a6445a9fff7e61e7605c4f
vinicius <vmm_7@hotmail.com>
2026-03-20 17:28:38 -0300

    refactor(middleware): mark unused next parameter with underscore

--- src/middlewares/errorHandler.middleware.js
+++ src/middlewares/errorHandler.middleware.js
@@ -4,7 +4,7 @@ import { AppError } from "../errors/AppError.js";
  * Middleware global de tratamento de erros.
  * Intercepta qualquer erro lançado (next(err)) e formata a resposta.
  */
-export function errorHandler(error, req, res, next) {
+export function errorHandler(error, req, res, _next) {
   // Log do erro para debug (pode ser melhorado com bibliotecas de log como Winston/Pino)
   console.error({ message: error.message, stack: error.stack });
 
